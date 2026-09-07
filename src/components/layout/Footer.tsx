343f63f232c813c795eb53587008c7e6a5c26955
knyavik <knyavik@gmail.com>
2026-09-07 19:18:37 +0300

    Modify footer styles and visibility of elements
    Updated font sizes and adjusted visibility of elements in the footer.

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -10,7 +10,7 @@ export function Footer() {
         <div className="flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between border-b border-white/5 pb-6">
           <div className="flex flex-wrap items-center gap-3">
             <BrandLogo size="sm" />
-            <span className="hidden sm:inline text-slate-600">•</span>
+            {/*<span className="hidden sm:inline text-slate-600">•</span>*/}
             <span className="text-xs text-slate-300 font-medium">
               Сервис честной селекции товаров wobuy.
             </span>
@@ -48,10 +48,10 @@ export function Footer() {
         </div>
 
         {/* Объединенная монолитная строка копирайта и защиты */}
-        <div className="flex flex-col gap-2 sm:flex-row sm:items-center sm:justify-between text-[6px] text-slate-400">
+        <div className="flex flex-col gap-2 sm:flex-row sm:items-center sm:justify-between text-[11px] text-slate-400">
           <div className="flex flex-wrap items-center gap-2">
             <span>© 2026 <strong className="font-bold text-white">wobuy.</strong> (домен wobuy.ru). Все права защищены.</span>
-            {/*<span className="hidden md:inline text-slate-600">•</span>*/}
+            <span className="hidden md:inline text-slate-600">•</span>
             <span className="inline-flex items-center gap-1 text-slate-300">
               <ShieldCheck className="h-3.5 w-3.5 text-[#00FF87]" />
               <span>Защищено независимым алгоритмом Анти-Фейк.</span>
